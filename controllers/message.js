e26a9c175064fe887a6aa4b0582a5d827ca274ac
Omar Tariq <otjutt@gmail.com>
2020-08-17 19:36:46 +0500

    Integrated wit and openweather and added general bot intelligence.

--- controllers/message.js
+++ controllers/message.js
@@ -1,23 +1,54 @@
 const { Message } = require('../models/index');
+const WitService = require('../service/wit-service');
+const BotService = require('../service/bot-service');
+const IntentWeather = require('../intent/intent-weather');
+const MessageService = require('../service/message-service');
 
 async function create(req, res) {
-    let message = null;
-    try {
-        message = await Message
-            .create({
-                sessionId: req.body.sessionId,
-                author: 'user',
-                message: req.body.message
-            });
+    let inputSessionId = req.body.sessionId;
+    let inputMessage = req.body.message;
 
-    } catch (e) {
-        console.log('Error! Exception in message create API.');
-        console.log(e.message);
-        return res.status(400).send({
-            message: "Something went wrong."
-        });
+    // Save user message in the database.
+    let data = await MessageService.create(
+        inputSessionId,
+        'user',
+        inputMessage
+    );
+    if (data.status === 'error') {
+        return res.status(data.code).send(data);
     }
-    return res.status(201).send(message);
+
+    // Send message to WIT to get cities and intents.
+    data = await WitService.process(inputMessage);
+    if (data.status === 'error') {
+        return res.status(data.code).send(data);
+    }
+
+    // Map intents in a hashmap.
+    let intentMap = data.data.intents;
+    let cities = data.data.cities;
+
+    // Check! If no intents are found then add bot response and return message.
+    if (intentMap.size === 0) {
+        data = await MessageService.create(
+            inputSessionId,
+            'bot',
+            'Sorry! I was not able to understand your intent.'
+        );
+        return res.status(data.code).send(data);
+    }
+
+    // Intent weather
+    if (intentMap.has('get_weather')) {
+        data = await IntentWeather.getWeather(inputSessionId, cities);
+        return res.status(data.code).send(data);
+    }
+
+    data = BotService.createMessage(
+        inputSessionId,
+        'Sorry! I was not able to understand what you said.',
+    );
+    return res.status(data.code).send(data);
 }
 
 async function list(req, res) {
